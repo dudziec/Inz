e5f4c91ed19e8a6e80ba0ffbb9e15a23daeb745d
dudziec <dudziecdamian@gmail.com>
2017-04-29 16:52:01 +0200

    Dodano implementację wzoru Haversine

--- inz/coordinates.py
+++ inz/coordinates.py
@@ -1,6 +1,9 @@
 from random import randint, random
-
+from math import asin, cos, pi, sin, radians, degrees
 from numpy.random.mtrand import randint
+
+_EARTH_RADIUS = 6371.01
+_EPSILON = 0.00000001
 
 
 class Coordinates:
@@ -11,15 +14,55 @@ class Coordinates:
         self.azimuth = 0
 
     def set_coordinates(self, coordinates, azimuth=0):
+        """
+
+        :param coordinates:
+        :param azimuth:
+        :return:
+        """
         self.latitude = coordinates[0]
         self.longitude = coordinates[1]
         self.azimuth = azimuth
 
-
     def set_random_buoy_coordinates(self, yacht_coordinates):
+        """
+
+        :param yacht_coordinates:
+        :return:
+        """
         shift = randint(0, 100) / 100000
         self.latitude = yacht_coordinates.latitude + shift
         self.longitude = yacht_coordinates.longitude + shift
 
     def __str__(self):
         return "{}, {}".format(self.latitude, self.longitude)
+
+    def set_coordinates_from_haversine_formula(self, initial_coordinates, azimuth, distance):
+        """ Funkcja obliczająca współrzędne za pomocą wzoru Haversine.
+
+        Funkcja
+
+        :param initial_coordinates: współrzędne jachtu
+        :param azimuth: kąt
+        :param distance: odległość
+        :return:
+        """
+        azimuth = (azimuth + 195) % 360
+
+        radians_latitude = radians(initial_coordinates.latitude)
+        radians_longitude = radians(initial_coordinates.longitude)
+
+        radians_azimuth = radians(azimuth)
+        radians_distance = distance / _EARTH_RADIUS
+
+        radians_latitude_tmp = asin(sin(radians_latitude) * cos(radians_distance)
+                                    + cos(radians_latitude) * sin(radians_distance) * cos(radians_azimuth))
+
+        if cos(radians_latitude_tmp) == 0 or abs(cos(radians_latitude_tmp)) < _EPSILON:
+            radians_longitude_tmp = radians_latitude_tmp
+        else:
+            radians_longitude_tmp = ((radians_longitude - asin(sin(radians_azimuth) * sin(radians_distance) /
+                                                               cos(radians_latitude_tmp)) + pi) % (2 * pi)) - pi
+
+        self.latitude = degrees(radians_latitude_tmp)
+        self.longitude = degrees(radians_longitude_tmp)
